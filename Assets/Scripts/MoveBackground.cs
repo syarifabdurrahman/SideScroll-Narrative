8d576727472ffaea589b34cb9907e5f05aed8e93
Syarif Abdurrahman <syarifabdr22@gmail.com>
2023-02-03 17:50:17 +0700

    Adding Parallax background

--- Assets/Scripts/MoveBackground.cs
+++ Assets/Scripts/MoveBackground.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class MoveBackground : MonoBehaviour
 {
-    
+    private float length;
+    private float startPos;
+    private GameObject cam;
+
+    [SerializeField] private float parallaxEffectSpeed;
+
+    private void Awake()
+    {
+        cam = GameObject.Find("CM vcam1");
+        startPos = transform.position.x;
+        length = gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffectSpeed));
+        float distance = (cam.transform.position.x - parallaxEffectSpeed);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+
+        if (temp < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
 }
